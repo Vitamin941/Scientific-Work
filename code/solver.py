875988ff653dca38f0b354a77219d35442daac33
Vitamin941 <stukov.biryuckov2017@yandex.ru>
2021-09-25 20:45:51 +0800

    update solver

--- code/solver.py
+++ code/solver.py
@@ -34,7 +34,7 @@ def X(time_moment=None, tau=tau):
     :return:
     """
 
-    def x(time_moment, T, ind: List[int]):
+    def x(time_moment_start, time_moment_end, ind: List[int]):
         """
         :param t:
         :param T:
@@ -44,7 +44,7 @@ def X(time_moment=None, tau=tau):
         if ind[0] == ind[1]:
             return 1
         else:
-            return time_moment - T
+            return time_moment_start - time_moment_end
 
     x11 = x(time_moment, tau, [1, 1])
     x12 = x(time_moment, tau, [1, 2])
@@ -61,26 +61,27 @@ def X(time_moment=None, tau=tau):
     ])
 
 
-def F_func(arg=None):
+def F_func(time_arg):
     """
     Функция возвращающая матрицу F
     """
-    return F * X(t1, arg) + F * X(t2, arg)
+    return F * X(time_moments.get("t1"), time_arg) + F * X(time_moments.get("t2"), time_arg)
 
 F_matr = {
     "tau": F_func(tau),
     "t": F_func(t),
-    "t0": F_func(t0)
+    "t0": F_func(time_moments.get("t0"))
 }
 
 a = F_matr.get("tau") * B
-b = X(T, tau) * B
+b = X(time_moments.get("T"), tau) * B
 H = a.col_join(b)
 Q = H * H.T
 
 def find_tetta_vector():
-    column_1 = alpha - F_matr.get("t0") * x_0 - integrate(F * X(t1, t),(t,t0_num,t1_num)) * f - integrate(F * X(t2, t),(t,t0_num,t2_num)) * f
-    column_2 = x_T - X(T, t0) * x_0 - integrate(X(T, t) * f, (t, t0_num, T_num))
+    column_1 = alpha - F_matr.get("t0") * x_0 - (integrate(F * X(time_moments.get("t1"), t),(t,t0_num,t1_num)) * f)\
+         - (integrate(F * X(time_moments.get("t2"), t),(t,t0_num,t2_num)) * f)
+    column_2 = x_T - X(time_moments.get("T"), time_moments.get("t0")) * x_0 - integrate(X(time_moments.get("T"), t) * f, (t, t0_num, T_num))
     return replace_syms_to_nums(column_1.col_join(column_2))
 
 tetta = find_tetta_vector()
